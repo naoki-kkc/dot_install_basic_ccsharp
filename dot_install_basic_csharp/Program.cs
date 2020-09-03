d6c01070ebe8baa57ca934f4873159a208589306
naoki-kkc <naoki2kuchi@gmail.com>
2020-09-03 13:50:41 +0900

    lesson 05 end

--- dot_install_basic_csharp/Program.cs
+++ dot_install_basic_csharp/Program.cs
@@ -7,6 +7,7 @@ namespace dot_install_basic_charp
         static void Main(string[] args)
         {
             /* lesson 04 */
+            /*
             // 変数
             string msg_val;
             msg_val = "hoge";
@@ -22,7 +23,24 @@ namespace dot_install_basic_charp
             // 確認する場合はコメントアウトを外す
             // msg_const = "bar"; // <- 定数は再代入できないのでエラーとなる
             // Console.WriteLine(msg_const);
+            */
+
+            /* lesson 05 */
+            // よく使われる型
+            string  str = "string"; // 文字列
+            char    c   = 'c';      // 文字
+            int     i   = 100;      // 整数値
+            double  d   = 1.234;    // 浮動小数点数
+            float   f   = 2.34f;    // 浮動小数点数(最後にfをつける)
+            bool    b   = true;     // 論理値型
+
+            //型推論 - varを使った変数宣言
+            var x = "string";               // 文字列を代入
+            Console.WriteLine(x.GetType()); // -> System.String
+            var y = 1;                      // 整数値を代入
+            Console.WriteLine(y.GetType()); // -> System.Int32
             
+
 
         }
     }
